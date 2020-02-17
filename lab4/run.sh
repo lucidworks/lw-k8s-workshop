18bef0aef03d3d34fa02492d06e9db47fc76b1d8
Timothy Potter <thelabdude@gmail.com>
2020-02-17 08:46:31 -0700

    Reorg labs a bit

--- lab4/run.sh
+++ lab4/run.sh
@@ -2,4 +2,4 @@
 
 kubectl run --generator=run-pod/v1 --image=us.gcr.io/solr-dev/gatling-qps:lab4 \
   --restart=Never \
-  --env="JAVA_OPTS=-Dqps.fusion.url=https://tjp23265.lucidworkstest.com -Dqps.app=lab4 -Dqps.rps=180" gatling-qps -- -s FusionQueryTraffic
+  --env="JAVA_OPTS=-Dqps.fusion.url=https://tjp23466.lucidworkstest.com -Dqps.app=lab4 -Dqps.rps=180 -Dqps.duration.mins=20" gatling-qps -- -s FusionQueryTraffic
