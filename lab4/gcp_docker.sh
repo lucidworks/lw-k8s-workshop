5f9a0ec41150bf49687da6e9781734bcbb1241a3
Timothy Potter <thelabdude@gmail.com>
2020-02-20 07:52:50 -0800

    set vars

--- lab4/gcp_docker.sh
+++ lab4/gcp_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gcloud auth configure-docker
-IMAGE="us.gcr.io/${LW_K8S_GCP_PROJECT}/gatling-qps:${LW_K8S_NAMESPACE}"
-docker tag "gatling-qps:${LW_K8S_NAMESPACE}" "${IMAGE}"
+IMAGE="us.gcr.io/${LW_K8S_GCP_PROJECT}/gatling-qps:lab4"
+docker tag "gatling-qps:lab4" "${IMAGE}"
 echo -e "\nPushing ${IMAGE} to GCP ..."
 docker push "${IMAGE}"
--- lab4/run.sh
+++ lab4/run.sh
@@ -1,5 +1,5 @@
 #docker container run --env JAVA_OPTS="-Dqps.fusion.url=https://fusiondemo.lucidworkstest.com -Dqps.app=datagen" gatling-qps:feb11 -s FusionQueryTraffic
 
-kubectl run --generator=run-pod/v1 --image=us.gcr.io/solr-dev/gatling-qps:lab4 \
+kubectl run --generator=run-pod/v1 --image=us.gcr.io/lw-support-team/gatling-qps:lab4 \
   --restart=Never \
-  --env="JAVA_OPTS=-Dqps.fusion.url=https://tjp23466.lucidworkstest.com -Dqps.app=lab4 -Dqps.rps=180 -Dqps.duration.mins=20" gatling-qps -- -s FusionQueryTraffic
+  --env="JAVA_OPTS=-Dqps.fusion.url=http://35.230.105.8:6764 -Dqps.app=lab4 -Dqps.rps=600 -Dqps.duration.mins=15 -Dqps.ramp.secs=45" gatling-qps -- -s FusionQueryTraffic
