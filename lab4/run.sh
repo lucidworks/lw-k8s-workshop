74b04a504547551d1565e642361e97a167c40543
Timothy Potter <thelabdude@gmail.com>
2020-02-25 16:34:15 -0700

    Update lab6

--- lab4/run.sh
+++ lab4/run.sh
@@ -1,5 +1,5 @@
 #docker container run --env JAVA_OPTS="-Dqps.fusion.url=https://fusiondemo.lucidworkstest.com -Dqps.app=datagen" gatling-qps:feb11 -s FusionQueryTraffic
 
-kubectl run --generator=run-pod/v1 --image=us.gcr.io/lw-support-team/gatling-qps:lab4 \
+kubectl run --generator=run-pod/v1 --image=us.gcr.io/${LW_K8S_GCP_PROJECT}/gatling-qps:lab4 \
   --restart=Never \
-  --env="JAVA_OPTS=-Dqps.fusion.url=http://35.230.105.8:6764 -Dqps.app=lab4 -Dqps.rps=600 -Dqps.duration.mins=15 -Dqps.ramp.secs=45" gatling-qps -- -s FusionQueryTraffic
+  --env="JAVA_OPTS=-Dqps.fusion.url=http://${LW_K8S_GATEWAY_IP}:6764 -Dqps.app=lab4 -Dqps.rps=800 -Dqps.duration.mins=60 -Dqps.ramp.secs=60" gatling-qps -- -s FusionQueryTraffic
